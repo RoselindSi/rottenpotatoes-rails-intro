96ccd891ac6c753b5b6eb69b4d51b573af4245dc
Roselind Si <xs2567@columbia.edu>
2025-09-28 15:59:22 +0000

    Part 1: filter movies by rating with default select-all

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+  # Returns all distinct rating values present in the DB
+  # Used by the view to render one checkbox per rating.
+  def self.all_ratings
+    select(:rating).distinct.order(:rating).pluck(:rating)
+  end
+
+  # Returns an ActiveRecord relation filtered by the given ratings_list.
+  # - If ratings_list is nil or empty, return all movies
+  def self.with_ratings(ratings_list)
+    return all if ratings_list.nil? || ratings_list.empty?
+    where(rating: ratings_list)
+  end
 end
